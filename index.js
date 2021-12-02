94e08ff8e08d068e42cc3e9f2f0e4f7544bd324c
Ishantyadav <>
2021-12-02 17:30:13 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var superman={
+  name:"superman",
+  power:"fligth",
+  costumecolor:"blue",
+  strength:1000,
+  stealth:0
+}
+var batman={
+  name:"batman",
+  power:"martial arts",
+  strength:400
+}
+console.log(superman.strength);
